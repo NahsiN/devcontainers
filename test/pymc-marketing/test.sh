0300b0e58681a7a4fd635b440dfa6277341e758a
Nishan Mann <nishan.singh.mann.casing969@slmails.com>
2026-04-05 09:23:43 -0400

    feat: add pymc-marketing devcontainer

---
+++ test/pymc-marketing/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd $(dirname "$0")
+source test-utils.sh
+
+# Template specific tests
+check "pymc" python -c "import pymc"
+check "pymc-extras" python -c "import pymc_extras"
+check "pymc-marketing" python -c "import pymc_marketing"
+check "claude" claude --version
+
+# Report result
+reportResults
