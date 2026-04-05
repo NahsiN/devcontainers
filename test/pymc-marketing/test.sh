b254c4e96e2abbd9fe4c4bc474a00859113f98c6
Nishan Mann <nishan.singh.mann.casing969@slmails.com>
2026-04-05 10:39:26 -0400

    chore: add git to pymc-marketing devcontainer

--- test/pymc-marketing/test.sh
+++ test/pymc-marketing/test.sh
@@ -6,6 +6,7 @@ source test-utils.sh
 check "pymc" python -c "import pymc"
 check "pymc-extras" python -c "import pymc_extras"
 check "pymc-marketing" python -c "import pymc_marketing"
+check "git" git --version
 check "claude" claude --version
 
 # Report result
